348a03b9114884dee9a88b68a6bd4b34ff949756
tanbro <tanbro@163.com>
2024-05-08 17:17:51 +0800

    edit: add some doc-comments

--- src/emoji_data/helpers.py
+++ src/emoji_data/helpers.py
@@ -6,12 +6,17 @@ __all__ = ["load_emoji_data", "unload_emoji_data"]
 
 
 def load_emoji_data():
+    """Load all emoji data from in-package data file.
+
+    Including Emoji characters and sequences
+    """
     EmojiCharacter.initial()
     make_emoji_regex_dict()
     EmojiSequence.initial()
 
 
 def unload_emoji_data():
+    """Release emoji data stored in memory"""
     EmojiSequence.release()
     clear_emoji_regex_dict()
     EmojiCharacter.release()
