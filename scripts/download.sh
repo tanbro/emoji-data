916bb84aa3cdc77d89b63363afbd2ab2a1b971f2
Liu Xue Yan <liu_xue_yan@foxmail.com>
2024-12-06 11:17:40 +0800

    build: 优化 emoji 数据下载脚本

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+set -eux
 
-dest="src/emoji_data/data"
+urls=(
+    https://www.unicode.org/Public/UCD/latest/ucd/emoji/emoji-data.txt
+    https://www.unicode.org/Public/UCD/latest/ucd/emoji/emoji-variation-sequences.txt
+    https://www.unicode.org/Public/emoji/latest/emoji-sequences.txt
+    https://www.unicode.org/Public/emoji/latest/emoji-zwj-sequences.txt
+    https://www.unicode.org/Public/emoji/latest/emoji-test.txt
+)
 
-wget -P $dest https://www.unicode.org/Public/UCD/latest/ucd/emoji/emoji-data.txt
-wget -P $dest https://www.unicode.org/Public/UCD/latest/ucd/emoji/emoji-variation-sequences.txt
-wget -P $dest https://www.unicode.org/Public/emoji/latest/emoji-sequences.txt
-wget -P $dest https://www.unicode.org/Public/emoji/latest/emoji-zwj-sequences.txt
-wget -P $dest https://www.unicode.org/Public/emoji/latest/emoji-test.txt
+for url in ${urls[@]}
+do
+  wget -cNP src/emoji_data/data $url
+done
